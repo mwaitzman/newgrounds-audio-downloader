c92cf9b35edb951d1fbbcf03e390469b2031b29a
mwaitzman <mwaitzman@outlook.com>
2022-08-15 00:40:27 -0400

    clean up program

--- src/main.rs
+++ src/main.rs
@@ -1,84 +1,53 @@
 #![feature(type_name_of_val)]
+use backoff::{retry, ExponentialBackoff};
+use derive_builder::Builder;
+use ego_tree::iter::Edge;
+use regex::Regex;
+use reqwest::{redirect::Policy, StatusCode, Url};
+use scraper::{node::Node, Html, Selector};
+use serde::Serialize;
 use std::{
 	io::Write,
 	path::{Path, PathBuf},
 	time::Duration,
 };
 
-use backoff::{retry, ExponentialBackoff};
-use derive_builder::Builder;
-use ego_tree::iter::Edge;
-use regex::Regex;
-/*
-mostly a port of https://github.com/Henri-J-Norden/py-newgrounds-downloader
-*/
-use reqwest::{redirect::Policy, StatusCode, Url};
-use scraper::{node::Node, Html, Selector};
-use serde::{Deserialize, Serialize};
-// use time::Date;
-
-// use clap::{crate_version, Arg, Command as ClapCommand};
 fn main() -> anyhow::Result<()> {
 	println!("Start of newgrounds-audio-downloader");
-
-	// let mut fi
+	//TODO: get from CLI
 	get_files("keosni391")?;
 	Ok(())
 }
 
-// fn download_file()
 pub fn get_files(username: &str) -> anyhow::Result<()> {
-	/*
-	def getFiles(username,folder=".\\",dlType="audio"):
-	matches = []
-	matches_genres = []
-	page_i = 1
-	more = True
-	while more:
-		url = f"https://{username}.newgrounds.com/{dlType}/page/{str(page_i)}"
-		print("Fetching '{}'...".format(url))
-		req = Request(url, headers={"User-agent":"Mozilla/5.0", "x-requested-with": "XMLHttpRequest"})
-		page = str(urlopen(req).read(),encoding="UTF-8")
-		matches += re.findall('<a href=.*?newgrounds\.com.*?'+dlType+'.*?listen.*?([0-9]+).*?title\=.*?\"(.+?)\\\\\">', page)
-		matches_genres += re.findall('detail-genre.*?(?:\s)+([ \w]+).*?div>', page)
-		more = SONGS_ON_PAGE_STRING in page
-		page_i += 1
-	print("Found {} songs.".format(str(len(matches))))
-	print(matches)
-	urls = ["https://www.newgrounds.com/audio/download/{}/".format(matches[i][0]) for i in range(len(matches))]
-	files = [folder+username+"\\"+matches_genres[i].replace("Song","").strip()+"\\"+matches[i][1]+".mp3" for i in range(len(matches))]
-	if not os.path.exists(folder+username):
-		os.mkdir(folder+username)
-
-	DL(urls,files)
-	*/
+	//TODO: get this stuff and more from the args (but keep dl_type as audio as the default and probably only supported download type (more would require name change and archive with notice of new crate if published))
 	let dl_type = "audio";
 	let dl_directory = "/mnt/ehdd/DH/music/artists/K-391/newgrounds";
 
 	let template = format!("https://{username}.newgrounds.com/{dl_type}/page/");
 	let mut all_matches = vec![];
+
 	//TODO: possibly scrape the songs' listen pages instead (probably more robust, and likely additional information can be retrieved from there) (TODO: check if likes, downloads, upload date, etc are already archived by us)
-	let mut all_matches_genres = vec![];
-	let mut i = 1;
+
+	let mut pages_i = 1;
 	let matches_re = Regex::new(
 		format!(
 			"<a href=.*?newgrounds\\.com.*?{dl_type}.*?listen.*?([0-9]+)\" class=\"item-audiosubmission \">"
 		)
 		.as_str(),
 	)?;
-	let matches_genres_re = Regex::new("detail-genre.*?(?:\\s)+([ \\w]+).*?div>")?;
 
 	let client = reqwest::blocking::ClientBuilder::new()
 		.redirect(Policy::custom(|attempt| {
+			//expected_example: "https://keosni391.newgrounds.com/audio/page/3/" => "https://keosni391.newgrounds.com/audio/page/3/?page=2", where the artist keosni391 has exactly 2 pages of songs
 			let original_url = attempt.previous()[0].clone();
 			if attempt.previous().len() != 1 {
 				panic!("multiple previous URLs!");
 			}
 			let redirected_url = attempt.url().clone();
 			println!("redirection of {} to {}", original_url, redirected_url);
-			// let url_page_number_capture_re = Regex::new()
-			// "https://keosni391.newgrounds.com/audio/page/3/" => "https://keosni391.newgrounds.com/audio/page/3/?page=2"
-			// this code is super messy and bad and could be refactored to be much better
+
+			// This code is super messy and bad and could be refactored to be much better
 			let query_string = redirected_url.query().unwrap();
 			let query_re = Regex::new("^page=(?P<page_number>\\d+)$").unwrap();
 			let query_page_number_cap = query_re
@@ -108,10 +77,11 @@ pub fn get_files(username: &str) -> anyhow::Result<()> {
 		.build()?;
 
 	'get_pages: loop {
-		let url = Url::parse(format!("{template}{i}").as_str())?;
+		let url = Url::parse(format!("{template}{pages_i}").as_str())?;
 		println!("fetching {url}");
 		let resp = client.get(url).send();
 		let content = match resp {
+			//CHECK: I think utf-8 is the default used by the library so I could just do `resp.text()` IIRC, although I'm not even sure the response's encoding is UTF-8 in the first place, so this might all be bad anyways
 			Ok(resp) => resp.text_with_charset("utf-8")?,
 			Err(e) => {
 				if e.is_redirect() {
@@ -127,22 +97,10 @@ pub fn get_files(username: &str) -> anyhow::Result<()> {
 				.captures_iter(&content)
 				.map(|item| item.get(0).unwrap().as_str().to_owned()),
 		);
-		all_matches_genres.extend(
-			matches_genres_re
-				.captures_iter(&content)
-				.map(|item| item.get(0).unwrap().as_str().to_owned()),
-		);
-		i += 1;
+		pages_i += 1;
 	}
-	// assert!(all_matches.len() == all_matches_genres.len());
-	println!("{} songs found", all_matches.len());
-	// all_matches
-	// 	.iter()
-	// 	.zip(all_matches_genres.iter())
-	// 	.enumerate()
-	// 	.for_each(|(index, (song, genre))| {
-	// 		println!("[{index}: {song:?} (genre: {genre:?}");
-	// 	});
+	println!("{} songs found ({pages_i} pages)", all_matches.len());
+
 	let song_ids = all_matches
 		.iter()
 		.map(|item| {
@@ -158,7 +116,7 @@ pub fn get_files(username: &str) -> anyhow::Result<()> {
 				.as_str()
 		})
 		.collect::<Vec<_>>();
-	// let urls = all_matches...
+
 	let files = song_ids
 		.iter()
 		.map(|id| Path::new(dl_directory).join(id))
@@ -169,13 +127,13 @@ pub fn get_files(username: &str) -> anyhow::Result<()> {
 		.map(|id| Url::parse(&format!("http://www.newgrounds.com/audio/download/{id}")).unwrap())
 		.collect();
 
-	//TODO: download metadata as well
 	let metadatas = get_metadata_for_ids(&song_ids)?;
 	let mut metadata_location = PathBuf::new();
 	metadata_location.push(dl_directory);
 	metadata_location.push("metadata.json");
 	let metadata_file = std::fs::File::create(metadata_location)?;
 	serde_json::to_writer_pretty(metadata_file, &metadatas)?;
+
 	download_files(urls, files)?;
 	Ok(())
 }
@@ -184,31 +142,22 @@ pub fn get_files(username: &str) -> anyhow::Result<()> {
 /// ```rs
 /// download_files([Url::parse("http://www.newgrounds.com/audio/download/626468").unwrap()],[Path::new("file.mp3").to_path_buf]);
 /// ```
-#[allow(unused_variables)]
 //TODO: make async
 pub fn download_files(urls: Vec<Url>, mut files: Vec<PathBuf>) -> anyhow::Result<()> {
 	assert!(urls.len() == files.len());
-	// urls.iter()
-	// 	.zip(files.iter())
-	// 	.for_each(|(url, file)| println!("{url} | {file:?}"));
-	//TODO: remove file field from program
+	//TODO: remove file field from program - new impl dependent on whether it's guaranteed that file names will always be present from the response and never conflict causing overwrites with different ones
 	urls.into_iter()
 		.zip(files.iter_mut())
 		.for_each(|(url, file)| {
 			let backoff = ExponentialBackoff::default();
 			let instant = std::time::Instant::now();
 			let op = || {
+				//TODO: overwrite the previous text instead of spamming the terminal with new progress messages
 				println!(
 					"backoff due to rate-limiting for url {url}: {:?} seconds",
 					instant.elapsed().as_secs()
 				);
-				// println!("Fetching {}", url);
-				// let mut resp = reqwest::blocking::get(url)?;
-
-				// let mut content = String::new();
-				// let _ = resp.read_to_string(&mut content);
-				// Ok(content)
-
+				//TODO: [re-]use a client instead
 				let response = reqwest::blocking::get(url.clone())
 					.map_err(backoff::Error::transient)
 					.unwrap();
@@ -221,6 +170,7 @@ pub fn download_files(urls: Vec<Url>, mut files: Vec<PathBuf>) -> anyhow::Result
 									if let Ok(seconds) = time_str.parse::<u64>() {
 										Some(Duration::from_secs(seconds))
 									} else if let Ok(systemtime) =
+									//CHECK: I'm not sure it's even possible for httpdate to parse this stuff. This code may be pure bloat
 										httpdate::parse_http_date(time_str)
 									{
 										println!(
@@ -293,21 +243,18 @@ pub fn download_files(urls: Vec<Url>, mut files: Vec<PathBuf>) -> anyhow::Result
 				}
 			}
 		});
-	// println!("{:?}", reqwest::blocking::get(urls[0].clone()).unwrap().headers());
-	// let response = reqwest::blocking::get(url)?;
-	// let mut file = std::fs::File::create(file_name)?;
-	// let mut content =  Cursor::new(response.bytes().await?);
-	// std::io::copy(&mut content, &mut file)?;
 	Ok(())
 }
 
 pub fn get_metadata_for_ids(ids: &[&str]) -> anyhow::Result<Vec<NewGroundsAudioMetadata>> {
-	macro_rules! extract_node_from_edge {
+	// both variants contain the same stuff which we want to handle the same way (I think, at least), and I do the same thing everywhere so this simple macro is helpful
+	macro_rules! extract_value_from_edge {
 		($edge:ident) => {
 			match $edge {
 				Edge::Open(node) => node,
 				Edge::Close(node) => node,
 			}
+			.value()
 		};
 	}
 	let base_metadata_url = Url::parse("https://www.newgrounds.com/audio/listen/")?;
@@ -316,38 +263,27 @@ pub fn get_metadata_for_ids(ids: &[&str]) -> anyhow::Result<Vec<NewGroundsAudioM
 		.map(|id| -> anyhow::Result<NewGroundsAudioMetadata> {
 			let url = base_metadata_url.clone().join(id)?;
 			println!("{}", url);
+			//TODO: (re-)use a client instead
 			let response = reqwest::blocking::get(url)?;
 			let html = Html::parse_document(&response.text()?);
 			let mut builder = NewGroundsAudioMetadataBuilder::default();
 			let selector = Selector::parse("#sidestats").unwrap();
-			// let stats_selectors = html.select(&selector);
-			// println!("found {} matching selectors", stats_selectors.count());
 			let mut stats_selector = html.select(&selector);
 			let sidestats_div = stats_selector.next().unwrap();
 			assert!(stats_selector.next() == None);
-			// let dt_selector = Selector::parse("dt").unwrap();
-			// let dd_selector = Selector::parse("dd").unwrap();
 			let whitespace_re = Regex::new(r"^\s+$").unwrap();
 			let pending_score_re = Regex::new(r"^\s*Waiting for \d+ more votes?\s*$").unwrap();
 			let mut sidestats_iter = sidestats_div.traverse();
-			#[cfg(not(feature = "a"))]
+			#[cfg(not(feature = "just_print_all_nodes"))]
 			{
 				'main: while let Some(child) = sidestats_iter.next() {
-					// both variants contain the same stuff
-					let node = extract_node_from_edge!(child);
-					// if let Some(raw_listens) = node.value()
-					// .as_element().unwrap()
-					// .select(&dd_selector).next() {
-					// 	println!("{raw_listens}");
-					// }
-
-					let value = node.value();
+					let value = extract_value_from_edge!(child);
 					if let Node::Element(element) = value {
 						if element.name() == "dt" {
 							if let Some(child) = sidestats_iter.next() {
-								let node = extract_node_from_edge!(child);
-								let value = node.value();
+								let value = extract_value_from_edge!(child);
 								if let Node::Text(text) = value {
+									#[allow(clippy::while_let_on_iterator)]
 									match &**text {
 										"Listens" => {
 											sidestats_iter.next(); // Listens clone (ending/closing part - applies to below too)
@@ -355,14 +291,12 @@ pub fn get_metadata_for_ids(ids: &[&str]) -> anyhow::Result<Vec<NewGroundsAudioM
 											sidestats_iter.next(); // whitespace
 											sidestats_iter.next(); // whitespace clone
 											if let Some(child) = sidestats_iter.next() {
-												let node = extract_node_from_edge!(child);
-												let value = node.value();
+												let value = extract_value_from_edge!(child);
 												if let Node::Element(element) = value {
 													if element.name() == "dd" {
 														if let Some(child) = sidestats_iter.next() {
-															let node =
-																extract_node_from_edge!(child);
-															let value = node.value();
+															let value =
+																extract_value_from_edge!(child);
 															if let Node::Text(text) = value {
 																println!("text: {:?}", text);
 																let text =
@@ -382,8 +316,8 @@ pub fn get_metadata_for_ids(ids: &[&str]) -> anyhow::Result<Vec<NewGroundsAudioM
 											sidestats_iter.next();
 											sidestats_iter.next();
 											while let Some(child) = sidestats_iter.next() {
-												let node = extract_node_from_edge!(child);
-												let value = node.value();
+												let value = extract_value_from_edge!(child);
+												//CHECK: can a macro be used for the other block of exactly duplicated and nearly exactly duplicated lines here?
 												if let Node::Text(text) = value {
 													let text = (**text).replace(',', "");
 													if let Ok(faves) = text.parse::<u64>() {
@@ -397,8 +331,7 @@ pub fn get_metadata_for_ids(ids: &[&str]) -> anyhow::Result<Vec<NewGroundsAudioM
 											sidestats_iter.next();
 											sidestats_iter.next();
 											while let Some(child) = sidestats_iter.next() {
-												let node = extract_node_from_edge!(child);
-												let value = node.value();
+												let value = extract_value_from_edge!(child);
 												if let Node::Text(text) = value {
 													let text = (**text).replace(',', "");
 													if let Ok(downloads) = text.parse::<u64>() {
@@ -412,8 +345,7 @@ pub fn get_metadata_for_ids(ids: &[&str]) -> anyhow::Result<Vec<NewGroundsAudioM
 											sidestats_iter.next();
 											sidestats_iter.next();
 											while let Some(child) = sidestats_iter.next() {
-												let node = extract_node_from_edge!(child);
-												let value = node.value();
+												let value = extract_value_from_edge!(child);
 												if let Node::Text(text) = value {
 													let text = (**text).replace(',', "");
 													if let Ok(votes) = text.parse::<u64>() {
@@ -427,15 +359,14 @@ pub fn get_metadata_for_ids(ids: &[&str]) -> anyhow::Result<Vec<NewGroundsAudioM
 											sidestats_iter.next();
 											sidestats_iter.next();
 											while let Some(child) = sidestats_iter.next() {
-												let node = extract_node_from_edge!(child);
-												let value = node.value();
+												let value = extract_value_from_edge!(child);
 												if let Node::Text(text) = value {
 													let text = (**text).replace(',', "");
 													if let Ok(score) = text.parse::<f64>() {
 														builder.score(ScoreType::Score(score));
 														continue 'main;
 													} else if pending_score_re.is_match(&text) {
-														builder.votes(None);
+														let text = text.trim().to_owned();
 														builder.score(ScoreType::Waiting(text));
 														builder.votes(None);
 														continue 'main;
@@ -444,15 +375,16 @@ pub fn get_metadata_for_ids(ids: &[&str]) -> anyhow::Result<Vec<NewGroundsAudioM
 											}
 										}
 										"Uploaded" => {
+											//WATCH: these calls might be able to skip over some information we want. Should change this to be more robust. This goes for the other calls in the upper match as well. Should check and handle them just in case
 											sidestats_iter.next();
 											sidestats_iter.next();
 											sidestats_iter.next();
 											let mut upload_date = String::new();
-											let mut c: u8 = 0;
+											// the date comes in two separate Texts, with no whitespace separating one Text from the other
+											let mut date_parts_found: u8 = 0;
 											while let Some(child) = sidestats_iter.next() {
 												sidestats_iter.next();
-												let node = extract_node_from_edge!(child);
-												let value = node.value();
+												let value = extract_value_from_edge!(child);
 												if let Node::Text(text) = value {
 													let text = &(**text);
 													if text.is_empty() {
@@ -461,38 +393,31 @@ pub fn get_metadata_for_ids(ids: &[&str]) -> anyhow::Result<Vec<NewGroundsAudioM
 													if whitespace_re.is_match(text) {
 														continue;
 													}
-													if c == 1 {
+													if date_parts_found == 1 {
 														upload_date.push(' ');
 													}
 													upload_date.push_str(text);
-													c += 1;
-												} else if c > 1 {
+													date_parts_found += 1;
+												} else if date_parts_found > 1 {
 													println!("upload_date: {}", &upload_date);
 													builder.uploaded(upload_date);
 													continue 'main;
 												}
 											}
 										}
-										_ => {
-											// println!();
-											// dbg!(text);
-											// println!();
-											// panic!();
-										}
+										_ => {}
 									}
 								}
 							}
 						}
 					}
-					// dbg!(node.value());
 				}
 			}
-			#[cfg(feature = "a")]
+			// this feature and code was used sometimes in developing. Could probably just remove it all but no real reason to do so just yet
+			#[cfg(feature = "just_print_all_nodes")]
 			{
 				while let Some(child) = sidestats_iter.next() {
-					let node = extract_node_from_edge!(child);
-
-					let value = node.value();
+					let value = extract_value_from_edge!(child);
 					dbg!(value);
 				}
 			}
@@ -504,31 +429,25 @@ pub fn get_metadata_for_ids(ids: &[&str]) -> anyhow::Result<Vec<NewGroundsAudioM
 		.collect::<Vec<_>>())
 }
 
-#[derive(Serialize, Deserialize, Builder, Debug)]
+#[derive(Serialize, Builder, Debug)]
 #[builder(derive(Debug))]
-// #[serde(rename...)]
 pub struct NewGroundsAudioMetadata {
 	listens: u64,
-	//CONSIDER: logging the users who favorited as well (endpoint example https://www.newgrounds.com/favorites/content/who/429941/3 (from dd a href id="faves_load"))
+	//CONSIDER: logging the users who favorited as well (endpoint example https://www.newgrounds.com/favorites/content/who/429941/3 (from <dd ...<a href .. id="faves_load"))
 	faves: u64,
 	downloads: u64,
+	// if there aren't enough votes, both the score and the votes aren't normal, with one not being displayed and the other displaying a message about it waiting for X votes
 	votes: Option<u64>,
 	score: ScoreType,
 	uploaded: String,
+	//TODO: the genre and tags
 	// genre: String,
 	// tags: Vec<String>,
 }
 
-#[derive(Serialize, Deserialize, Clone, Debug)]
+#[derive(Serialize, Clone, Debug)]
 pub enum ScoreType {
+	// an f32 might be sufficient but who really cares about the increased cost of using an f64 just to possibly lose less info
 	Score(f64),
 	Waiting(String),
-}
-#[cfg(test)]
-mod tests {
-	// Note this useful idiom: importing names from outer (for mod tests) scope.
-	// use super::*;
-
-	#[test]
-	fn test_download_files() {}
 }
