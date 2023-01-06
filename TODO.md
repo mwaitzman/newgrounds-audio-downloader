76881765c79b6c14a81d9cbd1f8e0e23fc99150a
mwaitzman <mwaitzman@outlook.com>
2023-01-05 20:50:23 -0500

    (August 2021): migrate to using lazy_regex
    now uses the lazy_regex crate for better performance.

--- TODO.md
+++ TODO.md
@@ -2,3 +2,5 @@ archive metadata (from e.g. https://www.newgrounds.com/audio/listen/516421 -> Cr
 make async? might not be worth it because rate limiting is so bad
 make it look nice - colored output, progress bars, overwrite previous output with updates instead of appending
 Replace all the unwraps
+
+update lazy_regex crate to use regex version 1.6 and make sure lazy_regex is actually using all performance features and is correct
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 use backoff::{retry, ExponentialBackoff};
 use derive_builder::Builder;
 use ego_tree::iter::Edge;
+use lazy_regex::*;
 use regex::Regex;
 use reqwest::{redirect::Policy, StatusCode, Url};
 use scraper::{node::Node, Html, Selector};
@@ -11,6 +12,14 @@ use std::{
 	path::{Path, PathBuf},
 	time::Duration,
 };
+
+static FILE_NAME_REGEX: Lazy<Regex> = lazy_regex!(r#"^attachment; filename="(?P<file_name>.+)"$"#);
+static QUERY_REGEX: Lazy<Regex> = lazy_regex!("^page=(?P<page_number>\\d+)$");
+static ORIGINAL_NUMBER_REGEX: Lazy<Regex> = lazy_regex!("page/(?P<page_number>\\d+)/?$");
+static URL_EXTRACTION_REGEX: Lazy<Regex> =
+	lazy_regex!(r#"^<a href="https://www.newgrounds.com/audio/listen/(?P<id>\d+)"#);
+static WHITESPACE_REGEX: Lazy<Regex> = lazy_regex!(r"^\s+$");
+static PENDING_SCORE_REGEX: Lazy<Regex> = lazy_regex!(r"^\s*Waiting for \d+ more votes?\s*$");
 
 fn main() -> anyhow::Result<()> {
 	println!("Start of newgrounds-audio-downloader");
@@ -30,7 +39,7 @@ pub fn get_files(username: &str) -> anyhow::Result<()> {
 	//TODO: possibly scrape the songs' listen pages instead (probably more robust, and likely additional information can be retrieved from there) (TODO: check if likes, downloads, upload date, etc are already archived by us)
 
 	let mut pages_i = 1;
-	let matches_re = Regex::new(
+	let matches_regex = Regex::new(
 		format!(
 			"<a href=.*?newgrounds\\.com.*?{dl_type}.*?listen.*?([0-9]+)\" class=\"item-audiosubmission \">"
 		)
@@ -49,14 +58,14 @@ pub fn get_files(username: &str) -> anyhow::Result<()> {
 
 			// This code is super messy and bad and could be refactored to be much better
 			let query_string = redirected_url.query().unwrap();
-			let query_re = Regex::new("^page=(?P<page_number>\\d+)$").unwrap();
-			let query_page_number_cap = query_re
+			// let query_re = Regex::new("^page=(?P<page_number>\\d+)$").unwrap();
+			let query_page_number_cap = QUERY_REGEX
 				.captures(query_string)
 				.unwrap()
 				.name("page_number")
 				.unwrap();
-			let original_number_re = Regex::new("page/(?P<page_number>\\d+)/?$").unwrap();
-			let original_url_page_number_cap = original_number_re
+			// let original_number_re = Regex::new("page/(?P<page_number>\\d+)/?$").unwrap();
+			let original_url_page_number_cap = ORIGINAL_NUMBER_REGEX
 				.captures(original_url.path())
 				.unwrap()
 				.name("page_number")
@@ -93,7 +102,7 @@ pub fn get_files(username: &str) -> anyhow::Result<()> {
 		};
 
 		all_matches.extend(
-			matches_re
+			matches_regex
 				.captures_iter(&content)
 				.map(|item| item.get(0).unwrap().as_str().to_owned()),
 		);
@@ -105,10 +114,10 @@ pub fn get_files(username: &str) -> anyhow::Result<()> {
 		.iter()
 		.map(|item| {
 			//example_usage: `<a href="https://www.newgrounds.com/audio/listen/429941" class="item-audiosubmission ">` => `https://www.newgrounds.com/audio/listen/429941`
-			let url_extraction_regex =
-				Regex::new(r#"^<a href="https://www.newgrounds.com/audio/listen/(?P<id>\d+)"#)
-					.unwrap();
-			url_extraction_regex
+			// let url_extraction_regex =
+			// 	Regex::new(r#"^<a href="https://www.newgrounds.com/audio/listen/(?P<id>\d+)"#)
+			// 		.unwrap();
+			URL_EXTRACTION_REGEX
 				.captures(item)
 				.unwrap()
 				.name("id")
@@ -170,7 +179,7 @@ pub fn download_files(urls: Vec<Url>, mut files: Vec<PathBuf>) -> anyhow::Result
 									if let Ok(seconds) = time_str.parse::<u64>() {
 										Some(Duration::from_secs(seconds))
 									} else if let Ok(systemtime) =
-									//CHECK: I'm not sure it's even possible for httpdate to parse this stuff. This code may be pure bloat
+										//CHECK: I'm not sure it's even possible for httpdate to parse this stuff. This code may be pure bloat
 										httpdate::parse_http_date(time_str)
 									{
 										println!(
@@ -203,8 +212,8 @@ pub fn download_files(urls: Vec<Url>, mut files: Vec<PathBuf>) -> anyhow::Result
 				.unwrap_or_else(|| panic!("no content-disposition header for {}", response.url()))
 				.to_str()
 				.unwrap();
-			let file_name_re = Regex::new(r#"^attachment; filename="(?P<file_name>.+)"$"#).unwrap();
-			let file_name = file_name_re
+			// let file_name_re = Regex::new(r#"^attachment; filename="(?P<file_name>.+)"$"#).unwrap();
+			let file_name = FILE_NAME_REGEX
 				.captures(file_name)
 				.unwrap_or_else(|| panic!("failed to extract file name from {file_name}"))
 				.name("file_name")
@@ -270,9 +279,9 @@ pub fn get_metadata_for_ids(ids: &[&str]) -> anyhow::Result<Vec<NewGroundsAudioM
 			let selector = Selector::parse("#sidestats").unwrap();
 			let mut stats_selector = html.select(&selector);
 			let sidestats_div = stats_selector.next().unwrap();
-			assert!(stats_selector.next() == None);
-			let whitespace_re = Regex::new(r"^\s+$").unwrap();
-			let pending_score_re = Regex::new(r"^\s*Waiting for \d+ more votes?\s*$").unwrap();
+			assert!(stats_selector.next().is_none());
+			// let whitespace_re = Regex::new(r"^\s+$").unwrap();
+			// let pending_score_re = Regex::new(r"^\s*Waiting for \d+ more votes?\s*$").unwrap();
 			let mut sidestats_iter = sidestats_div.traverse();
 			#[cfg(not(feature = "just_print_all_nodes"))]
 			{
@@ -365,7 +374,7 @@ pub fn get_metadata_for_ids(ids: &[&str]) -> anyhow::Result<Vec<NewGroundsAudioM
 													if let Ok(score) = text.parse::<f64>() {
 														builder.score(ScoreType::Score(score));
 														continue 'main;
-													} else if pending_score_re.is_match(&text) {
+													} else if PENDING_SCORE_REGEX.is_match(&text) {
 														let text = text.trim().to_owned();
 														builder.score(ScoreType::Waiting(text));
 														builder.votes(None);
@@ -390,7 +399,7 @@ pub fn get_metadata_for_ids(ids: &[&str]) -> anyhow::Result<Vec<NewGroundsAudioM
 													if text.is_empty() {
 														continue;
 													}
-													if whitespace_re.is_match(text) {
+													if WHITESPACE_REGEX.is_match(text) {
 														continue;
 													}
 													if date_parts_found == 1 {
